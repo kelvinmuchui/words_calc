40a8f18da005dcc4e6ec8b492f0dd8606c4d8d40
kelvinmuchui <muchuikelvin423@gmail.com>
2016-06-15 17:18:12 +0300

    commited more

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello():
 	return "Kelvin muchui"
 
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+
 
 if __name__ =='__main__':
 	app.run()
