d2a45c6b95a987c7badab744e695019c7442e860
kelvinmuchui <muchuikelvin423@gmail.com>
2016-06-16 19:45:02 +0300

    add psycopg

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+mport os
 from flask import Flask
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
 def hello():
-	return "Kelvin muchui"
+    return "Hello World!"
+
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
 
+if __name__ == '__main__':
+    app.run()
 
-
-if __name__ =='__main__':
-	app.run()
